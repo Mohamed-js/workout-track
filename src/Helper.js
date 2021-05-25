4e864096e01295d5d6101be80b55ba1d286dbf59
Mohammed <atefhamosa@gmail.com>
2021-05-25 16:46:00 +0200

    Fix API endpoints in requests

--- src/Helper.js
+++ src/Helper.js
@@ -1,89 +1,82 @@
+const baseURL = 'https://diagnoser-proxy.herokuapp.com/http://workout-track-api.herokuapp.com/api/v1';
+
 export const signup = async (credits) => {
-  const respond = await fetch(
-    'https://diagnoser-proxy.herokuapp.com/http://workout-track-api.herokuapp.com/api/v1/users/sign-up',
-    {
-      headers: { 'Content-Type': 'application/json' },
-      method: 'post',
-      body: JSON.stringify(credits),
-    },
-  )
+  const respond = await fetch(`${baseURL}/users`, {
+    headers: { 'Content-Type': 'application/json' },
+    method: 'post',
+    body: JSON.stringify(credits),
+  })
     .then((res) => res.json())
     .then((data) => data);
   return respond;
 };
 
 export const signin = async (credits) => {
-  const respond = await fetch(
-    'https://diagnoser-proxy.herokuapp.com/http://workout-track-api.herokuapp.com/api/v1/users/sign-in',
-    {
-      headers: { 'Content-Type': 'application/json' },
-      method: 'post',
-      body: JSON.stringify(credits),
-    },
-  )
+  const respond = await fetch(`${baseURL}/sessions`, {
+    headers: { 'Content-Type': 'application/json' },
+    method: 'post',
+    body: JSON.stringify(credits),
+  })
     .then((res) => res.json())
     .then((data) => data);
   return respond;
 };
 
-export const newMovement = async (userID, movementID) => {
-  const respond = await fetch(
-    'https://diagnoser-proxy.herokuapp.com/http://workout-track-api.herokuapp.com/api/v1/movements/new',
-    {
-      headers: { 'Content-Type': 'application/json' },
-      method: 'post',
-      body: JSON.stringify({ user_id: userID, movement_id: movementID }),
+export const newMovement = async (token, movementID) => {
+  const respond = await fetch(`${baseURL}/tracked_movements`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: token,
     },
-  )
+    method: 'post',
+    body: JSON.stringify({ movement_id: movementID }),
+  })
     .then((res) => res.json())
     .then((data) => data);
   return respond;
 };
 
-export const newRecord = async (userID, movementID, movementCount) => {
-  const respond = await fetch(
-    'https://diagnoser-proxy.herokuapp.com/http://workout-track-api.herokuapp.com/api/v1/user/records/new',
-    {
-      headers: { 'Content-Type': 'application/json' },
-      method: 'post',
-      body: JSON.stringify({
-        user_id: userID,
-        movement_id: movementID,
-        movement_count: movementCount,
-      }),
+export const newRecord = async (token, movementID, movementCount) => {
+  const respond = await fetch(`${baseURL}/records`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: token,
     },
-  )
+    method: 'post',
+    body: JSON.stringify({
+      movement_id: movementID,
+      movement_count: movementCount,
+    }),
+  })
     .then((res) => res.json())
     .then((data) => data);
   return respond;
 };
 
-export const movementUserTopscore = async (userID, movementID) => {
-  const respond = await fetch(
-    `https://diagnoser-proxy.herokuapp.com/http://workout-track-api.herokuapp.com/api/v1/user/${userID}/${movementID}/topscore`,
-  )
+export const movementUserTopscore = async (token, movementID) => {
+  const respond = await fetch(`${baseURL}/records/${movementID}`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+  })
     .then((res) => res.json())
     .then((data) => data);
   return respond;
 };
 
-export const updateProfile = async (
-  userID,
-  Weight,
-  Height,
-) => {
-  const respond = await fetch(
-    'https://diagnoser-proxy.herokuapp.com/http://workout-track-api.herokuapp.com/api/v1/user/profile',
-    {
-      headers: { 'Content-Type': 'application/json' },
-      method: 'post',
-      body: JSON.stringify({
-        user_id: userID,
-        weight: Weight,
-        height: Height,
-      }),
+export const updateProfile = async (token, Weight, Height) => {
+  const respond = await fetch(`${baseURL}/users/update`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: token,
     },
-  )
+    method: 'put',
+    body: JSON.stringify({
+      weight: Weight,
+      height: Height,
+    }),
+  })
     .then((res) => res.json())
     .then((data) => data);
   return respond;
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,36 +1,38 @@
-export const fetchUntrackedMovements = (id) => (dispatch) => {
-  fetch(
-    `https://diagnoser-proxy.herokuapp.com/http://workout-track-api.herokuapp.com/api/v1/user/${id}/untracked-movements`,
-    {
-      headers: { 'Content-Type': 'application/json' },
+const baseURL = 'https://diagnoser-proxy.herokuapp.com/http://workout-track-api.herokuapp.com/api/v1';
+
+export const fetchUntrackedMovements = (token) => (dispatch) => {
+  fetch(`${baseURL}/untracked_movements`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: token,
     },
-  )
+  })
     .then((res) => res.json())
     .then((data) => {
       dispatch({ type: 'STORE_MOVEMENTS', payload: data });
     });
 };
 
-export const fetchTrackedMovements = (id) => (dispatch) => {
-  fetch(
-    `https://diagnoser-proxy.herokuapp.com/http://workout-track-api.herokuapp.com/api/v1/user/${id}/tracked-movements`,
-    {
-      headers: { 'Content-Type': 'application/json' },
+export const fetchTrackedMovements = (token) => (dispatch) => {
+  fetch(`${baseURL}/tracked_movements`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: token,
     },
-  )
+  })
     .then((res) => res.json())
     .then((data) => {
       dispatch({ type: 'STORE_USER_MOVEMENTS', payload: data });
     });
 };
 
-export const fetchUserRecords = (id) => (dispatch) => {
-  fetch(
-    `https://diagnoser-proxy.herokuapp.com/http://workout-track-api.herokuapp.com/api/v1/user/${id}/records`,
-    {
-      headers: { 'Content-Type': 'application/json' },
+export const fetchUserRecords = (token) => (dispatch) => {
+  fetch(`${baseURL}/records`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: token,
     },
-  )
+  })
     .then((res) => res.json())
     .then((data) => {
       dispatch({ type: 'STORE_USER_RECORDS', payload: data });
